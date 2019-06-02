6156accb62137f0839c8b25c7b7a1bed42270bb8
YuanFY <maple.janes@maplejanesdeMacBook-Pro.local>
2019-06-02 16:23:13 +0800

    二分查找法

---
+++ src/main/java/com/yuanfy/demo/utils/BinarySearchUtil.java
@@ -0,0 +1,116 @@
+package com.yuanfy.demo.utils;
+
+/**
+ * @author maple.yuan
+ * @date 2019-05-26 16:53
+ */
+public class BinarySearchUtil {
+
+    /**
+     * 简单二分查找法
+     */
+    public static int simpleBinarySearch(int[] arr, int findValue) {
+        if (arr.length < 1) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] > findValue) {
+                high = mid - 1;
+            } else if (arr[mid] < findValue) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 求x的平方根
+     * @param x
+     * @return
+     */
+    public static int sqrt(int x) {
+        if (x == 1) {
+            return x;
+        }
+        int low = 1;
+        int high = x;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            long value = (long)mid * mid;
+            if (value == x) {
+                return mid;
+            }
+            if (value > x) {
+                value = (long)(mid-1)*(mid-1);
+                if ( value <= x) {
+                    return mid - 1;
+                }
+                high = mid - 1;
+            } else {
+                value = (long)(mid+1)*(mid+1);
+                if ( value > x) {
+                    return mid;
+                } else if (value == x){
+                    return mid + 1;
+                }
+                low = mid + 1;
+            }
+        }
+        return 0;
+    }
+
+    public static int findFirstEquals(int[] arr, int value) {
+        if (arr.length < 1) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (arr[mid] == value) {
+                if (mid > 0 && arr[mid-1] == value) {
+                    high = mid - 1;
+                } else {
+                    return mid;
+                }
+            } else if (arr[mid] > value) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int getIndex(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                // 如果右边是顺序的且最大值都小于target的话，那么就可能在左边
+                if (nums[mid] <= nums[high] && target > nums[high]) {
+                    high = mid - 1;
+                    continue;
+                }
+                low = mid + 1;
+                continue;
+            }
+            // 如果左边是顺序的且最小值都大于target的话，那么就可能在右边
+            if (nums[mid] >= nums[low] && nums[low] > target) {
+                low = mid + 1;
+                continue;
+            }
+            high = mid - 1;
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/com/yuanfy/demo/utils/BinarySearchUtilTest.java
@@ -0,0 +1,67 @@
+package com.yuanfy.demo.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author maple.yuan
+ * @date 2019-05-26 20:25
+ */
+public class BinarySearchUtilTest {
+
+    @Test
+    public void test() {
+        int[] arr = {3, 3, 4, 5, 6, 9};
+        int idx = BinarySearchUtil.simpleBinarySearch(arr, 1);
+        Assert.assertEquals(idx, -1);
+        idx = BinarySearchUtil.simpleBinarySearch(arr, 6);
+        Assert.assertEquals(idx, 4);
+    }
+
+    @Test
+    public void test_sqrt() {
+        int sqrt = BinarySearchUtil.sqrt(1);
+        Assert.assertEquals(sqrt, 1);
+        sqrt = BinarySearchUtil.sqrt(3);
+        Assert.assertEquals(sqrt, 1);
+        sqrt = BinarySearchUtil.sqrt(4);
+        Assert.assertEquals(sqrt, 2);
+        sqrt = BinarySearchUtil.sqrt(26);
+        Assert.assertEquals(sqrt, 5);
+        sqrt = BinarySearchUtil.sqrt(101);
+        Assert.assertEquals(sqrt, 10);
+        sqrt = BinarySearchUtil.sqrt(10);
+        Assert.assertEquals(sqrt, 3);
+        sqrt = BinarySearchUtil.sqrt(2147395599);
+        Assert.assertEquals(sqrt, 46339);
+    }
+
+    @Test
+    public void test_findFisrtEquals() {
+        int[] arr = {1, 3, 3, 3, 5, 6, 9};
+        int idx = BinarySearchUtil.findFirstEquals(arr, 1);
+        Assert.assertEquals(idx, 0);
+        idx = BinarySearchUtil.findFirstEquals(arr, 3);
+        Assert.assertEquals(idx, 1);
+    }
+
+    @Test
+    public void test_getIndex() {
+        int[] arr = {7,8,9,2,3,4,5,6};
+        int idx = BinarySearchUtil.getIndex(arr, 5);
+        Assert.assertEquals(idx, 6);
+        idx = BinarySearchUtil.getIndex(arr, 8);
+        Assert.assertEquals(idx, 1);
+        idx = BinarySearchUtil.getIndex(arr, 10);
+        Assert.assertEquals(idx, -1);
+
+        int[] arr1 = {3,2};
+        idx = BinarySearchUtil.getIndex(arr1, 2);
+        Assert.assertEquals(idx, 1);
+
+        int[] arr2 = {4, 5, 6, 7, 0, 1, 2};
+        idx = BinarySearchUtil.getIndex(arr2, 0);
+        Assert.assertEquals(idx, 4);
+    }
+    
+}
